1b93b7f6461bf9e1b68ae15bd940fc8792c223b2
VadimLevkovich <vadum13531@gmail.com>
2021-05-12 23:44:15 +0300

    Fix tsconfig

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {
     FastifyAdapter,
     NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { AppModule } from './app.module.js';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(

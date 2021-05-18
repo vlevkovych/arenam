33b1efd2490f99824ede9e7832489648aef9c5ce
vlevkovych <vadym.levkovych@gmail.com>
2021-05-18 21:26:04 +0300

    Split graphql and app configurations into different directories

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppConfigModule } from './config/app-config.module';
+import { AppConfigModule } from './config/app/app-config.module';
+import { GraphqlConfigModule } from './config/graphql/graphql-config.module';
 
 @Module({
-    imports: [AppConfigModule],
+    imports: [AppConfigModule, GraphqlConfigModule],
 })
 export class AppModule {}

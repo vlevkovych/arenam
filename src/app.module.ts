72bf3a4ba57f8d6c8523db51038c0646dda93d96
Vadym Levkovych <38792455+vlevkovych@users.noreply.github.com>
2021-05-25 12:19:27 +0300

    Сhange fastify platform to express (#8)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppConfigModule } from './config/app/app-config.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [AppConfigModule],
+    imports: [
+        AppConfigModule,
+        GraphQLModule.forRoot({
+            autoSchemaFile: true,
+        }),
+        UserModule,
+    ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import type { NestFastifyApplication } from '@nestjs/platform-fastify';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import type { AppConfigService } from './config/app/app-config.service';
 import { EnvironmentVariables } from './config/app/env.type';
 
 const bootstrap: () => Promise<void> = async () => {
-    const app = await NestFactory.create<NestFastifyApplication>(
-        AppModule,
-        new FastifyAdapter(),
-    );
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
     const appConfigService: AppConfigService = app.get('AppConfigService');
     await app.listen(
         appConfigService.get(EnvironmentVariables.port),

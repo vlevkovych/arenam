6a277989bb133106b48ea266f6a26424b3618d40
vlevkovych <vadym.levkovych@gmail.com>
2021-05-25 13:04:24 +0300

    Install dependencies and setup postgres in docker-compose

--- src/config/app/env.type.ts
+++ src/config/app/env.type.ts
@@ -1,9 +1,15 @@
 export enum EnvironmentVariables {
     port = 'APP_PORT',
     host = 'APP_HOST',
+    dbUsername = 'DB_USERNAME',
+    dbPassword = 'DB_PASSWORD',
+    dbDatabaseName = 'DB_DATABASE_NAME',
 }
 
 export interface EnvType {
     [EnvironmentVariables.port]: number;
     [EnvironmentVariables.host]: string;
+    [EnvironmentVariables.dbUsername]: string;
+    [EnvironmentVariables.dbPassword]: string;
+    [EnvironmentVariables.dbDatabaseName]: string;
 }

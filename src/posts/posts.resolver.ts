fe1147ab6276ada0e4cd6ad0643ac93b2ddaf7ca
vlevkovych <vadym.levkovych@gmail.com>
2021-05-28 17:28:28 +0300

    Add createPost mutation

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { User } from '@prisma/client';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+import { CreatePostInput } from './dto/create-post.input';
+import { PostsService } from './posts.service';
 
 @Resolver()
-export class PostsResolver {}
+export class PostsResolver {
+    public constructor(private readonly postsService: PostsService) {}
+
+    @Mutation(() => String)
+    @UseGuards(GqlAuthGuard)
+    public async createPost(
+        @Args('input') input: CreatePostInput,
+        @CurrentUser() creator: User,
+    ): Promise<string> {
+        return this.postsService.createPost(input, creator);
+    }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import type { User } from '@prisma/client';
+import { PrismaService } from '../config/prisma/prisma.service';
+import type { CreatePostInput } from './dto/create-post.input';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    public constructor(private readonly prisma: PrismaService) {}
+
+    public async createPost(
+        input: CreatePostInput,
+        creator: User,
+    ): Promise<string> {
+        await this.prisma.post.create({
+            data: {
+                body: input.body,
+                creator: creator.id,
+                title: input.title,
+            },
+        });
+        return 'Post successfully created';
+    }
+}

b9e5d3c784bbe1e222f57797c4032d85cdb764bc
vlevkovych <vadym.levkovych@gmail.com>
2021-05-28 16:56:48 +0300

    Add Post and PostRating models to schema.prisma, make migration

---
+++ src/config/prisma/migrations/20210528135237_implementing_posts/migration.sql
@@ -0,0 +1,33 @@
+-- CreateEnum
+CREATE TYPE "RatingStatus" AS ENUM ('neutral', 'upvoted', 'downvoted');
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR NOT NULL,
+    "body" VARCHAR NOT NULL,
+    "creator" INTEGER NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PostRating" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "postId" INTEGER NOT NULL,
+    "rating" "RatingStatus" NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD FOREIGN KEY ("creator") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PostRating" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PostRating" ADD FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;

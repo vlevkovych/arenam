02f38ddd81b932bc788bdf7d46fe394061ded5ef
vlevkovych <vadym.levkovych@gmail.com>
2021-06-03 16:06:49 +0300

    Create payloads for Auth

---
+++ src/auth/dto/login.payload.ts
@@ -0,0 +1,16 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
+import { MutationPayload } from '../../common/graphql/interfaces/mutation-payload';
+import { UserError } from '../../common/graphql/types/user-error';
+
+@ObjectType({ implements: () => [MutationPayload] })
+export class LoginPayload implements MutationPayload {
+    @Field(() => [UserError])
+    public errors!: UserError[];
+
+    @Field(() => Boolean)
+    public isLoginSuccessful!: boolean;
+
+    @Field(() => String)
+    public jwtToken!: string;
+}

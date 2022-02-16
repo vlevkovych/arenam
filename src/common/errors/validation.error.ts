48c141fc6440d46f93ade18e6c104c3045ff92c6
vlevkovych <vadym.levkovych@gmail.com>
2022-02-16 22:40:57 +0200

    refactoring: remove unused class

--- src/common/errors/validation.error.ts
+++
@@ -1,9 +0,0 @@
-export class ValidationError extends Error {
-    public fieldName: string;
-
-    public constructor(message: string, fieldName: string) {
-        super(message);
-        this.message = message;
-        this.fieldName = fieldName;
-    }
-}

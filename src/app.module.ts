cbf8c8aa829af12069dfb0bd50519906b0a744db
vlevkovych <vadym.levkovych@gmail.com>
2022-02-16 19:31:50 +0200

    refactoring: add error handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,19 @@ import { PostsModule } from './posts/posts.module';
 import { RatingModule } from './rating/rating.module';
 import { UserModule } from './user/user.module';
 
+import type { GraphQLError, GraphQLFormattedError } from 'graphql';
+
 @Module({
     imports: [
         AppConfigModule,
         GraphQLModule.forRoot({
             autoSchemaFile: 'schema.gql',
+            formatError: (error: GraphQLError) => {
+                const graphQLFormattedError: GraphQLFormattedError = {
+                    message: error.message,
+                };
+                return graphQLFormattedError;
+            },
         }),
         UserModule,
         AuthModule,
